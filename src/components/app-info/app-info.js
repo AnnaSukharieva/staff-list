b9430021f4cda510b3f9fa4656670a39ce80e4bd
Anna <anyasukhareva03@gmail.com>
2023-01-31 22:22:47 +0200

    create template

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+	return (
+		<div className="app-info">
+			<h1> Accounting of employees in the company </h1>
+			<h2>  - the total number of staff</h2>
+			<h2>  employee(s) will receive the award </h2>
+		</div>
+	)
+}
+
+export default AppInfo;
---
+++ src/components/search-pannel/search-pannel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPannel = () => {
+	return (
+		<input
+			type="text"
+			className="form-control search-input"
+			placeholder="Search employee"/>
+	)
+}
+
+export default SearchPannel;
